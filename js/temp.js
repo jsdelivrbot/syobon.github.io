9858aa04524f93b1353739b4ab74aa072cc9e8fa
DAFU <34666601+kikiriko200@users.noreply.github.com>
2018-04-21 20:23:12 +0900

    Update temp.js

--- js/temp.js
+++ js/temp.js
@@ -1,1 +1,1 @@
-$(function(){$("#header").load("/syobon.github.io/temp/headmini.html");$("#footer").load("/syobon.github.io/temp/footermini.html")});
+$(function(){$("#header").load("/syobon.github.io/temp/headmini.html");$("#footer").load("/syobon.github.io/temp/footmini.html")});
